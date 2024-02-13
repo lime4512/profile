b07a5089c5da90eeb0fa1549b16cb75d4767db8b
Alex Kruglov <lime4512@mail.ru>
2024-02-14 00:41:12 +0300

    add projectData and method map

--- src/components/project/ProjectList.tsx
+++ src/components/project/ProjectList.tsx
@@ -1,41 +1,20 @@
 import '../../style/projectList.scss'
 import { ProjectCard } from './ProjectCard'
+import { projectData } from '../../data/projectData.ts'
 export const ProjectList = () => {
 	return (
 		<div className='project__container'>
 			<div className='project__content'>
-				<ProjectCard
-					title='CryptoBrain'
-					text='CryptoBrain is a crypto application that allows users to view information about various cryptocurrencies and easily buy and sell cryptocurrencies.'
-					stack={['React', 'SCSS']}
-					img='/projectImg/cryptoBrain.jpg'
-					urlCode='https://github.com/lime4512/CryptoBrain'
-					order={false}
-				/>
-				<ProjectCard
-					title='CryptoBrain'
-					text='CryptoBrain is a crypto application that allows users to view information about various cryptocurrencies and easily buy and sell cryptocurrencies.'
-					stack={['React', 'SCSS']}
-					img='/projectImg/cryptoBrain.jpg'
-					urlCode='https://github.com/lime4512/CryptoBrain'
-					order={true}
-				/>
-				<ProjectCard
-					title='CryptoBrain'
-					text='CryptoBrain is a crypto application that allows users to view information about various cryptocurrencies and easily buy and sell cryptocurrencies.'
-					stack={['React', 'SCSS']}
-					img='/projectImg/cryptoBrain.jpg'
-					urlCode='https://github.com/lime4512/CryptoBrain'
-					order={false}
-				/>
-				<ProjectCard
-					title='CryptoBrain'
-					text='CryptoBrain is a crypto application that allows users to view information about various cryptocurrencies and easily buy and sell cryptocurrencies.'
-					stack={['React', 'SCSS']}
-					img='/projectImg/cryptoBrain.jpg'
-					urlCode='https://github.com/lime4512/CryptoBrain'
-					order={true}
-				/>
+				{projectData.map(item => (
+					<ProjectCard
+						title={item.title}
+						text={item.text}
+						stack={item.stack}
+						img={item.img}
+						urlCode={item.urlCode}
+						order={item.order}
+					/>
+				))}
 			</div>
 		</div>
 	)
---
+++ src/data/projectData.ts
@@ -0,0 +1,34 @@
+export const projectData = [
+	{
+		title: 'CryptoBrain',
+		text: 'CryptoBrain is a crypto application that allows users to view information about various cryptocurrencies and easily buy and sell cryptocurrencies.',
+		stack: ['React', 'SCSS'],
+		img: '/projectImg/cryptoBrain.jpg',
+		urlCode: 'https://github.com/lime4512/CryptoBrain',
+		order: false,
+	},
+	{
+		title: 'MovieList',
+		text: 'MovieList is a site that allows users to view movies in different categories and complete information on each, also add movies to favorites.',
+		stack: ['React', 'SCSS', 'Next'],
+		img: '/projectImg/movieList.jpg',
+		urlCode: 'https://github.com/lime4512/Movie-List',
+		order: true,
+	},
+	{
+		title: 'TechShop',
+		text: 'TechShop is an online equipment store where you can buy various equipment and orders are sent to the seller’s telegram.',
+		stack: ['React', 'SCSS'],
+		img: '/projectImg/Tech-Shop.jpg',
+		urlCode: 'https://github.com/lime4512/Tech-Shop',
+		order: false,
+	},
+	{
+		title: 'CryptoProfile',
+		text: 'CryptoProfile is a site for tracking the rate of cryptocurrencies, also for adding your coins to cash and displaying a diagram of the amount of currency.',
+		stack: ['React', 'SCSS', 'MobX'],
+		img: '/projectImg/crypto-profile.jpg',
+		urlCode: 'https://github.com/lime4512/Crypto-Profile',
+		order: true,
+	},
+]
