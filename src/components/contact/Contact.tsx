898febedeb5350e4789d6295885c0bfd9d721da0
Alex Kruglov <lime4512@mail.ru>
2024-02-14 19:22:19 +0300

    add contactCard

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,34 +1,34 @@
 import '../../style/contact.scss'
-import { notification } from 'antd'
+import { ContactCard } from './ContactCard'
 export const Contact = () => {
-	const copyTachMail = () => {
-		navigator.clipboard.writeText('lime451212@gmail.com')
-		openNotificationWithIcon()
-	}
-
-	const [api, contextHolder] = notification.useNotification()
-
-	const openNotificationWithIcon = () => {
-		api['success']({
-			message: 'Mail lime451212@gmail.com copied',
-		})
-	}
 	return (
 		<section className='contact' id='contact'>
-			{contextHolder}
 			<div className='contact__content-text'>
 				<h4 className='contact__subTitle'>CONTACT</h4>
 				<h2 className='contact__title'>Get In Touch</h2>
 			</div>
-			<div className='contact__content'>
-				<img className='contact__content__img' src='/mail.svg' alt='mail' />
-				<div className='contact__mail'>
-					<h3 className='contact__mail__title'>Mail</h3>
-					<p className='contact__mail__name'>lime451212@gmail.com</p>
-				</div>
-				<button className='contact__mail__copy' onClick={copyTachMail}>
-					<img src='/content_copy.svg' alt='mail' />
-				</button>
+			<div className='contact__content__list'>
+				<ContactCard
+					title='Location'
+					name='Russia'
+					img='/location.svg'
+					copyName=''
+					copy={false}
+				/>
+				<ContactCard
+					title='Mail'
+					name='lime451212@gmail.com'
+					img='/mail.svg'
+					copyName='lime451212@gmail.com'
+					copy={true}
+				/>
+				<ContactCard
+					title='Telegram'
+					name='@lime4512'
+					img='/public/telegram.png'
+					copyName='https://t.me/lime4512'
+					copy={true}
+				/>
 			</div>
 		</section>
 	)
---
+++ src/components/contact/ContactCard.tsx
@@ -0,0 +1,49 @@
+import { FunctionComponent } from 'react'
+import '../../style/contactCard.scss'
+import { notification } from 'antd'
+
+interface Props {
+	title: string
+	name: string
+	img: string
+	copyName: string
+	copy: boolean
+}
+
+export const ContactCard: FunctionComponent<Props> = ({
+	title,
+	name,
+	img,
+	copyName,
+	copy,
+}) => {
+	const copyTach = () => {
+		navigator.clipboard.writeText(copyName)
+		openNotificationWithIcon()
+	}
+
+	const [api, contextHolder] = notification.useNotification()
+
+	const openNotificationWithIcon = () => {
+		api['success']({
+			message: `${copyName} copied`,
+		})
+	}
+	return (
+		<div className='contact__content'>
+			{contextHolder}
+			<img className='contact__content__img' src={img} alt={title} />
+			<div className='contact__mail'>
+				<h3 className='contact__mail__title'>{title}</h3>
+				<p className='contact__mail__name'>{name}</p>
+			</div>
+			{copy ? (
+				<button className='contact__mail__copy' onClick={copyTach}>
+					<img src='/content_copy.svg' alt='' />
+				</button>
+			) : (
+				<></>
+			)}
+		</div>
+	)
+}
